6249f993654df51467900b3cef16f73186bebd14
Jarred Sumner <jarred@jarredsumner.com>
2019-03-13 17:42:52 -0700

    great

--- packages/codeblog-previewer/webpack.config.js
+++ packages/codeblog-previewer/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
+    filename: "[name].[contenthash].js",
     globalObject: "self"
   },
   target: "web",
