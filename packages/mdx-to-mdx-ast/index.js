d63b62c9d73a2a05788ea9ca9d3111152c726fbd
Jarred Sumner <jarred@jarredsumner.com>
2019-03-16 21:17:47 -0700

    Update index.js

--- packages/mdx-to-mdx-ast/index.js
+++ packages/mdx-to-mdx-ast/index.js
@@ -18,38 +18,50 @@ const processor = unified()
   .use(markdown)
   .use(esSyntax);
 
-module.exports = async function(mdxString) {
+const handleMDXAst = function(node => {
+  try {
+    // Parse each node with Acorn JSX parser (powers Babel)
+    const jsxCheck = jsxParser.parse(node.value, {
+      sourceType: "module"
+    });
+    if (jsxCheck.body.length > 1) {
+      return jsxCheck.body.map(node => {
+        return handleMultiProgramNodes(node);
+      });
+    }
+    if (jsNodeIsImport(jsxCheck.body[0])) {
+      return [Object.assign({}, node, { type: "import" })];
+    }
+    if (jsNodeIsExport(jsxCheck.body[0])) {
+      return [Object.assign({}, node, { type: "export" })];
+    }
+    if (jsNodeIsJsxElement(jsxCheck.body[0])) {
+      return [Object.assign({}, node, { type: "jsx" })];
+    }
+  } catch (e) {
+    if (!(e instanceof SyntaxError)) {
+      console.error(e);
+    }
+    // Ignore nodes that are not parseable as JSX
+  }
+
+  return [node];                            
+});
+
+
+module.exports.mdxAst = async function(mdxString) {
   const markdownAstWithTokenizedEsSyntax = processor.parse(mdxString);
   const partialTree = await unified().run(markdownAstWithTokenizedEsSyntax);
-  const mdxAst = flatMap(partialTree, node => {
-    try {
-      // Parse each node with Acorn JSX parser (powers Babel)
-      const jsxCheck = jsxParser.parse(node.value, {
-        sourceType: "module"
-      });
-      if (jsxCheck.body.length > 1) {
-        return jsxCheck.body.map(node => {
-          return handleMultiProgramNodes(node);
-        });
-      }
-      if (jsNodeIsImport(jsxCheck.body[0])) {
-        return [Object.assign({}, node, { type: "import" })];
-      }
-      if (jsNodeIsExport(jsxCheck.body[0])) {
-        return [Object.assign({}, node, { type: "export" })];
-      }
-      if (jsNodeIsJsxElement(jsxCheck.body[0])) {
-        return [Object.assign({}, node, { type: "jsx" })];
-      }
-    } catch (e) {
-      if (!(e instanceof SyntaxError)) {
-        console.error(e);
-      }
-      // Ignore nodes that are not parseable as JSX
-    }
+  const mdxAst = flatMap(partialTree, handleMDXAst);
 
-    return [node];
-  });
+  return mdxAst;
+};
+
+
+module.exports.mdxAstSync = async function(mdxString) {
+  const markdownAstWithTokenizedEsSyntax = processor.parse(mdxString);
+  const partialTree = unified().runSync(markdownAstWithTokenizedEsSyntax);
+  const mdxAst = flatMap(partialTree, handleMDXAst);
 
   return mdxAst;
 };
