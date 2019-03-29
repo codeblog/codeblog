9ce435f7bff18cf32488a547fae504159788bd8c
Jarred Sumner <jarred@jarredsumner.com>
2019-03-28 22:00:39 -0700

    bumpity

--- packages/codeblog-build-npm-package/dist/index.js
+++ packages/codeblog-build-npm-package/dist/index.js
@@ -1300,7 +1300,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
                 jsx = _mdx.default.sync(_children.filter(function (line) {
                   return !/^import/.test(line);
                 }).join("\n"), {
-                  mdPlugins: [[// Removes front-matter from Markdown output
+                  remarkPlugins: [[// Removes front-matter from Markdown output
                   __webpack_require__(23), {
                     type: "yaml",
                     marker: "-",
